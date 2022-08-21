8a96ca050121dc7477f30a9dc4dbac66f3761969
loremipsum <loremipsum.xu@hotmail.com>
2022-08-22 00:36:04 +0800

    feat: allow prepend string with string appender

--- string/appender.go
+++ string/appender.go
@@ -1,40 +1,98 @@
 package string
 
-
 import (
-"strings"
+	"fmt"
+	"unsafe"
 )
 
-type StringAppender struct {
-	stringBuilder strings.Builder
+// Appender is similar to strings.Appender, but is used to produce pathnames
+// given path components in reverse order (from leaf to root). This is useful
+// in the common case where a filesystem is represented by a tree of named
+// nodes, and the path to a given node must be produced by walking upward from
+// that node to a given root.
+type Appender struct {
+	buf     []byte
+	start   int
+	needSep bool
 }
 
-func NewStringAppender(str string) *StringAppender {
-	stringBuilder := strings.Builder{}
-	stringBuilder.WriteString(str)
-	appender := &StringAppender{
-		stringBuilder: strings.Builder{},
+// Reset resets the Builder to be empty.
+func (b *Appender) Reset() {
+	b.start = len(b.buf)
+	b.needSep = false
+}
+
+// Len returns the number of accumulated bytes.
+func (b *Appender) Len() int {
+	return len(b.buf) - b.start
+}
+
+func (b *Appender) needToGrow(n int) bool {
+	return b.start < n
+}
+
+func (b *Appender) grow(n int) {
+	newLen := b.Len() + n
+	var newCap int
+	if len(b.buf) == 0 {
+		newCap = 64 // arbitrary
+	} else {
+		newCap = 2 * len(b.buf)
 	}
-	return appender
-}
-
-func (appender *StringAppender) Append(stringToAppend string) error {
-	_, err := appender.stringBuilder.WriteString(stringToAppend)
-	return err
-}
-func (appender *StringAppender) AppendIf(condition bool, stringToAppend string) error {
-	if condition {
-		_, err := appender.stringBuilder.WriteString(stringToAppend)
-		return err
+	for newCap < newLen {
+		newCap *= 2
+		if newCap == 0 {
+			panic(fmt.Sprintf("required length (%d) causes buffer size to overflow", newLen))
+		}
 	}
-	return nil
+	newBuf := make([]byte, newCap)
+	copy(newBuf[newCap-b.Len():], b.buf[b.start:])
+	b.start += newCap - len(b.buf)
+	b.buf = newBuf
 }
 
-func (appender *StringAppender) AppendNewLine() error {
-	_, err := appender.stringBuilder.WriteString("\n")
-	return err
+// PrependComponent prepends the given path component to b's buffer. A path
+// separator is automatically inserted if appropriate.
+func (b *Appender) PrependComponent(pc string) {
+	if b.needSep {
+		b.PrependByte('/')
+	}
+	b.PrependString(pc)
+	b.needSep = true
 }
 
-func (appender *StringAppender) ToString() string {
-	return appender.stringBuilder.String()
+// PrependString prepends the given string to b's buffer.
+func (b *Appender) PrependString(str string) {
+	if b.needToGrow(len(str)) {
+		b.grow(len(str))
+	}
+	b.start -= len(str)
+	copy(b.buf[b.start:], str)
+}
+
+// PrependByte prepends the given byte to b's buffer.
+func (b *Appender) PrependByte(c byte) {
+	if b.needToGrow(1) {
+		b.grow(1)
+	}
+	b.start--
+	b.buf[b.start] = c
+}
+
+// AppendString appends the given string to b's buffer.
+func (b *Appender) AppendString(str string) {
+	if b.needToGrow(len(str)) {
+		b.grow(len(str))
+	}
+	oldStart := b.start
+	b.start -= len(str)
+	copy(b.buf[b.start:], b.buf[oldStart:])
+	copy(b.buf[len(b.buf)-len(str):], str)
+}
+
+// String returns the accumulated string. No other methods should be called
+// after String.
+func (b *Appender) String() string {
+	buffer := b.buf[b.start:]
+	return *(*string)(unsafe.Pointer(&buffer))
 }
