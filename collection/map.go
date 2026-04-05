9471fadca3b52443527b505bac319118f90ff0cd
xuruifeng <xuruifeng@unity-drive.com>
2026-04-05 11:29:21 +0800

    feat: values

--- collection/map.go
+++ collection/map.go
@@ -9,3 +9,13 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	}
 	return keys
 }
+
+// Values returns a slice containing all values from the given map.
+// The order of values is not guaranteed and may vary between iterations.
+func Values[K comparable, V any](m map[K]V) []V {
+	values := make([]V, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	return values
+}
