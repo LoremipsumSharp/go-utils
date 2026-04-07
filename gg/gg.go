f2f1c128ad6976ef05aae9576e8c917a008dc2f5
xuruifeng <xuruifeng@unity-drive.com>
2026-04-07 17:28:34 +0800

    feat: toType

--- gg/gg.go
+++ gg/gg.go
@@ -1,6 +1,9 @@
 package gg
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func If[T any](cond bool, a, b T) T {
 	if cond {
@@ -11,4 +14,33 @@ func If[T any](cond bool, a, b T) T {
 
 func IsZero[T any](v T) bool {
 	return reflect.ValueOf(&v).Elem().IsZero()
+}
+
+func ToType[T any](v any) (T, bool) {
+	var zero T
+	val := reflect.ValueOf(v)
+	if !val.IsValid() {
+		return zero, false
+	}
+	targetType := reflect.TypeOf(zero)
+	if val.Type() == targetType {
+		return v.(T), true
+	}
+	if val.Type().ConvertibleTo(targetType) {
+		return val.Convert(targetType).Interface().(T), true
+	}
+	return zero, false
+}
+
+func MustToType[T any](v any) T {
+	if res, ok := ToType[T](v); ok {
+		return res
+	}
+	var zero T
+	targetType := reflect.TypeOf(zero)
+	val := reflect.ValueOf(v)
+	if val.IsValid() {
+		panic(fmt.Sprintf("cannot convert value of type %s to %s", val.Type(), targetType))
+	}
+	panic(fmt.Sprintf("cannot convert invalid value to %s", targetType))
 }
